ec9add5f8f6eefcdcf6d46438d75a9c3203e32e7
Wang X.Q <wxq788@gmail.com>
2016-10-26 10:40:51 +0800

    test3

---
+++ test2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int toes = 10;
+    int chengJi;
+    int heJi;
+    
+    chengJi = toes * toes;
+    heJi = toes + toes;
+
+    printf("toes 的值为：%d;\n", toes);
+    printf("两个 toes 的和为：%d;\n", heJi);
+    printf("两个 toes 的乘积为：%d;\n", chengJi);
+}
---
+++ test3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 3; j > i; j--) {
+            printf ("Smile!");
+        }
+        printf ("\n");
+    }
+}
