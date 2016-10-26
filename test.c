dba4d91eeb7e0d70abd4ce2476e522b14e4bc80d
Wang X.Q <wxq788@gmail.com>
2016-10-26 10:16:22 +0800

    test

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main( void )
+{
+    char a[] = "For he's a jolly good fellow!";
+    char b[] = "Which nobody can deny!";
+
+    for( int i = 0; i < 3; i++ )
+    {
+        printf("%s\n", a);
+    }
+
+    printf("%s\n", b);
+}
